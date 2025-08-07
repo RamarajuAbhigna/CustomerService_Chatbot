314aaef71a7628ffb14729bc495c3a778dc64804
Ramaraju Abhigna <ramarajuabhigna@gmail.com>
2025-08-07 19:40:52 +0530

    updated config_voice.py

--- utils/config_voice.py
+++ utils/config_voice.py
@@ -8,4 +8,4 @@ load_dotenv()
 class VoiceConfig:
     TTS_RATE   = int(os.getenv("TTS_RATE", 180))          # words / min
     TTS_VOLUME = float(os.getenv("TTS_VOLUME", 0.9))
-    MIC_TIMEOUT = int(os.getenv("MIC_TIMEOUT", 7))
+    MIC_TIMEOUT = int(os.getenv("MIC_TIMEOUT", 9))
